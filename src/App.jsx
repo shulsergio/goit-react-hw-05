eadba26674360043ce5069883179638a196474aa
Sergey Shulga <shul.sergio@gmail.com>
2024-09-08 18:27:31 +0300

    v 1.0 start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>HW5</p>
+  
+      </div>
+
+    </>
+  )
+}
+
+export default App
