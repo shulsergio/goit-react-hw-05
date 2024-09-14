cf4cf1ad38439cab04ba17698b33179febcc960f
Sergey Shulga <shul.sergio@gmail.com>
2024-09-14 11:08:27 +0300

    v 1.5 fix err
    v 1.5 fix err

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,6 @@ import "./App.css";
 import { lazy, Suspense } from "react";
 import Loader from "../Loader/Loader";
 import { Navigations } from "../Navigations/Navigations";
-// import MovieDetailsPage from "../../pages/MovieDetailsPage";
-// import MoviePage from "../../pages/MoviesPage";
-// import NotFoundPage from "../../pages/NotFoundPage";
-// import MovieCast from "../MovieCast/MovieCast";
-// import MovieReviews from "../MovieReviews/MovieReviews";
 
 const HomePage = lazy(() => import("../../pages/HomePage"));
 const MoviePage = lazy(() => import("../../pages/MoviesPage"));
@@ -28,8 +23,6 @@ const App = () => {
             <Route path="cast" element={<MovieCast />} />
             <Route path="reviews" element={<MovieReviews />} />
           </Route>
-
-          <Route path="/movies/:movieId/cast" element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
